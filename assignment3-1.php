a7612ef708c05a136768defd03548dfb1b11f589
jthiggins74 <122487017+jthiggins74@users.noreply.github.com>
2023-01-18 11:29:31 -0600

    Add files via upload

---
+++ assignment3-1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h1>Assignment 3-1</h1>
+
+    <?php
+$yourName = "Jeff";
+$number1 = "1";
+$number2 = "2";
+
+
+echo $yourName
+$num1=15;  
+$num2=30;  
+$total=$x+$y;  
+echo "Sum: ",$total;  
+?>
+
+</body>
+
+</html>
