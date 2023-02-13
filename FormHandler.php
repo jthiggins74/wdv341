f0c7ee893255a6ac72dcfa92d50ac21d35b94928
Jeffery Higgins <122487017+jthiggins74@users.noreply.github.com>
2023-02-13 06:21:52 -0600

    Add files via upload

--- FormHandler.php
+++ FormHandler.php
@@ -11,10 +11,10 @@
 
 <h3>Instructions</h3>
 <ol>
-  <li>Place the file name 'demonstrateFormHandler.php' in the action attribute of your form. This is using the default pathname and will look for this file in the same location as the form.html page. You may place server side processes in their own folder on the server.  It is common to use a folder called 'files' which contains server side processes. In that case you would include the pathname in your action attribute. Example: action='files/demonstrateFormHandler.php' </li>
+  <li>Place the file name 'FormHandler.php' in the action attribute of your form. This is using the default pathname and will look for this file in the same location as the form.html page. You may place server side processes in their own folder on the server.  It is common to use a folder called 'files' which contains server side processes. In that case you would include the pathname in your action attribute. Example: action='files/demonstrateFormHandler.php' </li>
   <li>Move your form.html page AND this page to your host server.</li>
   <li>Use your browser to locate and run the form.html page on your host server. </li>
-  <li>Complete the form and click Submit.</li>
+  <li>Complete the form and clinock Submit.</li>
 </ol>
 <p>The table below displays the 'name=value' pairs that were entered on the form and processed on the server.  This page is a result of that server side process.</p>
 <p>The <strong>Field Name</strong> column contains the value of the name attribute for each field on the form. <em>Example: &lt;input name=&quot;first_name&quot;&gt;</em>  This displays what you coded into the HTML. NOTE: If you do not have a name attribute for a field OR if the name attribute does not have a value the form will NOT send the data to the server.</p>
@@ -33,7 +33,9 @@
 	} 
 	echo "</table>";
 	echo "<p>&nbsp;</p>";
-
+	$msg = "First Line of text\nSecond line of text";
+	$msg = wordwrap($msg,70);
+	mail("higginsglobalinvestments@gmail.com", "My subject",$msg);
 ?>
 
 </body>
